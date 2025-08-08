981c98ee1b51ee094d668c4e365ba9b10a56784c
Yash Ram <yashramt@gmail.com>
2025-08-08 11:11:41 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
