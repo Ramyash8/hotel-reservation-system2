b1e9d5da35f6b9d4309461b57dd107d180414d9b
Yash Ram <yashramt@gmail.com>
2025-08-15 18:08:26 +0000

    Error: Invalid src prop (https://images.pexels.com/photos/159896/hot-air

--- next.config.ts
+++ next.config.ts
@@ -52,6 +52,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.pexels.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
