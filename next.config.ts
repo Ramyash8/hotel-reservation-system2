2353d2222bd2b2461635a3842531079b8dc23460
Yash Ram <yashramt@gmail.com>
2025-08-15 15:49:00 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'gos3.ibcdn.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
